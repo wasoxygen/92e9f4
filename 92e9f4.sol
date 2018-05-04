4de3abe7b229265fbbf8e2995471ebd5a097ccb4
wasoxygen <wasoxygen@yahoo.com>
2018-05-04 14:13:35 -0400

    language updates; preset destination address

--- 92e9f4.sol
+++ 92e9f4.sol
@@ -1,49 +1,51 @@
 pragma solidity ^0.4.23;
 
-contract DominantAssuranceContract {
-    address owner;
+contract Fundski_2018 {
+    address sponsor;
     uint256 deadline;
     uint256 goal;
-    uint8 percentagePayoff;
+    uint8 bonusPercent;
     mapping(address => uint256) public balanceOf;
     uint256 totalPledges;
 
-    constructor(uint256 numberOfDays, uint256 _goal, uint8 _percentagePayoff) public payable {
-        owner = msg.sender;
+    constructor(uint256 numberOfDays, uint256 _goal, uint8 _bonusPercent) public payable {
+        sponsor = msg.sender;
         deadline = now + (numberOfDays * 1 days);
         goal = _goal;
-        percentagePayoff = _percentagePayoff;
+        bonusPercent = _bonusPercent;
         balanceOf[msg.sender] = msg.value;
     }
 
     function pledge(uint256 amount) public payable {
-        require(now < deadline, "The campaign is over.");
-        require(msg.value == amount, "The amount is incorrect.");
-        require(msg.sender != owner, "The owner cannot pledge.");
+        require(now < deadline, "Sorry, this campaign is over.");
+        require(msg.value == amount, "The amount pledged does not match the amount sent.");
+        require(msg.sender != sponsor, "The sponsor cannot pledge.");
 
-        uint256 payoff = amount * percentagePayoff / 100;
-        if (payoff > balanceOf[owner]) {
-            payoff = balanceOf[owner];
+        uint256 supporterBonus = amount * bonusPercent / 100;
+        if (supporterBonus > balanceOf[sponsor]) {
+            supporterBonus = balanceOf[sponsor];
         }
-        balanceOf[owner] -= payoff;
-        balanceOf[msg.sender] += amount+payoff;
+        balanceOf[sponsor] -= supporterBonus;
+        balanceOf[msg.sender] += amount+supporterBonus;
         totalPledges += amount;
     }
 
-    function claimFunds() public {
-        require(now >= deadline, "The campaign is not over.");
-        require(totalPledges >= goal, "The funding goal was not met.");
-        require(msg.sender == owner, "Only the owner may claim funds.");
-
-        msg.sender.transfer(address(this).balance);
+    function consummate() public {
+        require(now >= deadline, "The campaign is not over yet!");
+        require(totalPledges >= goal, "Alas, the funding goal was not met. Use getRefund() to recover pledges and bonus.");
+        
+        address hubski = 0x5fcc0ba549683f3211f933997b68c09b6f92e9f4;
+        hubski.transfer(address(this).balance);
     }
 
     function getRefund() public {
-        require(now >= deadline, "The campaign is still active.");
-        require(totalPledges < goal, "Funding goal was met.");
+        require(now >= deadline, "The campaign is not over yet!");
+        require(totalPledges < goal, "Good news, the  goal was met. Use consummate() to transfer pledges to the destination.");
 
         uint256 amount = balanceOf[msg.sender];
         balanceOf[msg.sender] = 0;
         msg.sender.transfer(amount);
     }
+    
+    //todo: handle long-abandoned pledges
 }
